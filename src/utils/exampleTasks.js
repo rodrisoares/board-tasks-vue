132905d9b9b210d9563bf5b0c9708fcaa6e717c9
Rodrigo Soares <diihhsoares2@gmail.com>
2023-03-30 18:10:02 -0300

    criando componente de board

---
+++ src/utils/exampleTasks.js
@@ -0,0 +1,44 @@
+export default {
+  tasks: {
+    name: 'tasks',
+    title: 'Tasks ✏️',
+    tasks: [
+      {
+        text: 'Exemplo task 1',
+      },
+      {
+        text: 'Exemplo task 2',
+      },
+    ],
+  },
+
+  dev: {
+    name: 'dev',
+    title: 'Desenvolvimento 🔨',
+    tasks: [
+      {
+        text: 'Exemplo task 3',
+      },
+    ],
+  },
+
+  testes: {
+    name: 'testes',
+    title: 'Testes 🔒',
+    tasks: [
+      {
+        text: 'Exemplo task 4',
+      },
+    ],
+  },
+
+  done: {
+    name: 'done',
+    title: 'Finalizados ✅',
+    tasks: [
+      {
+        text: 'Exemplo task 5',
+      },
+    ],
+  },
+};
---
+++ src/views/Board.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="board-view view">
+    <h1 class="display-3 text-uppercase text-center">Board</h1>
+
+    <main>
+      <div class="board" v-for="(board, index) in boards" :key="index">
+        <div class="board-title">
+          <h3>
+            {{ board.title }}
+          </h3>
+
+          <button @click="addTask(board.name)">
+            <i class="fas fa-plus"></i>
+          </button>
+        </div>
+
+        <Container
+          group-name="trello"
+          class="drag-container"
+          @drag-start="handleDragStart(board.name, $event)"
+          @drop="handleDrop(board.name, $event)"
+          :get-child-payload="getChildPayload"
+          :drop-placeholder="{ className: 'drop-placeholder' }"
+          drag-class="task-ghost"
+          drop-class="task-ghost-drop"
+        >
+          <Draggable v-for="(task, index) in board.tasks" :key="index">
+            <div class="task">
+              <h3>{{ task.text }}</h3>
+              <button @click="deleteTask(board.name, index)">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          </Draggable>
+        </Container>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { Container, Draggable } from 'vue-smooth-dnd';
+import exampleTasks from '../utils/exampleTasks';
+
+export default {
+  components: {
+    Container,
+    Draggable,
+  },
+
+  beforeMount() {
+    let dataFromStorage = localStorage.getItem('boards');
+
+    if (!dataFromStorage) {
+      this.boards = exampleTasks;
+      localStorage.setItem('boards', JSON.stringify(this.boards));
+    } else {
+      this.boards = JSON.parse(dataFromStorage);
+    }
+  },
+
+  data: () => ({
+    boards: {},
+
+    draggingTask: {
+      lane: '',
+      index: -1,
+      taskData: {},
+    },
+  }),
+
+  methods: {
+    handleDragStart(lane, dragResult) {
+      const { payload, isSource } = dragResult;
+      if (isSource) {
+        this.draggingTask = {
+          index: payload.index,
+          taskData: { ...this.boards[lane].tasks[payload.index] },
+        };
+      }
+    },
+
+    handleDrop(lane, dropResult) {
+      const { removedIndex, addedIndex } = dropResult;
+
+      if (lane == this.draggingTask.lane && removedIndex === addedIndex) {
+        return;
+      }
+
+      if (removedIndex !== null) {
+        this.boards[lane].tasks.splice(removedIndex, 1);
+      }
+
+      if (addedIndex !== null) {
+        this.boards[lane].tasks.splice(addedIndex, 0, this.draggingTask.taskData);
+      }
+
+      this.saveBoardsOnStorage();
+    },
+
+    getChildPayload(index) {
+      return {
+        index,
+      };
+    },
+
+    deleteTask(boardName, taskIndex) {
+      this.$swal
+        .fire({
+          title: 'Deseja excluir a task?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Sim',
+          cancelButtonText: 'Não',
+          reverseButtons: true,
+        })
+        .then(result => {
+          if (result.isConfirmed) {
+            this.boards[boardName].tasks.splice(taskIndex, 1);
+            this.saveBoardsOnStorage();
+          }
+        });
+    },
+
+    async addTask(boardName) {
+      const { value: taskText } = await this.$swal.fire({
+        title: 'Adicionar tarefa',
+        input: 'text',
+        inputPlaceholder: 'Nome da task',
+        showCancelButton: true,
+        reverseButtons: true,
+        confirmButtonText: 'adicionar',
+        cancelButtonText: 'cancelar',
+        inputValidator: value => {
+          if (!value) {
+            return 'Você deve adicionar um nome à task';
+          }
+        },
+      });
+
+      if (taskText) {
+        this.boards[boardName].tasks.push({ text: taskText });
+        this.saveBoardsOnStorage();
+      }
+    },
+
+    saveBoardsOnStorage() {
+      localStorage.setItem('boards', JSON.stringify(this.boards));
+    },
+  },
+};
+</script>
+
+<style></style>
